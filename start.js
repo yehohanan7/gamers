4c00a61a21da00777c6125298984a4db4551a1a8
unknown <johnpradeep.v@BG3WS0088.SSLTUIODC.LOCAL>
2013-06-17 12:51:12 +0530

    Initial commit

---
+++ start.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path');
+
+app.configure(function () {
+    app.use(express.static(path.join(__dirname, '.')));
+    app.use(app.router);
+});
+
+var server = http.createServer(app);
+
+server.listen(7777);
