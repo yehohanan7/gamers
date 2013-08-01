e9baec2241ad31259b37b18d0f75baa7d5ae15be
Pradeep <pradeep.kumar44@gmail.com>
2013-08-01 19:29:11 +0530

    added Spring support for gamers project.

---
+++ src/main/java/com/gamers/springConfig/WebConfig.java
@@ -0,0 +1,23 @@
+package com.gamers.springconfig;
+ 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+ 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.gamers.controller")
+public class WebConfig {
+ 
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = 
+                    new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+     
+}
