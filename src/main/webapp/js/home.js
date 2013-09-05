fa4325a8d75de1508682efc8e662c1bd5b84ebe0
John <yehohanan7@gmail.com>
2013-09-05 20:19:47 +0100

    basic angular carousel added

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -1,16 +1,10 @@
-//noinspection JSUnresolvedVariable,JSCheckFunctionSignatures
-angular.module('home', []).
-  directive('carousel', function () {
-    return {
-      restrict: 'E',
-      transclude: true,
-      scope: {},
-      controller: function ($scope, $element) {
-        $scope.select = function (pane) {
-
-        }
-      },
-      template: '<div>carousels</div>',
-      replace: true
-    };
-  })
+function CarouselCtrl($scope) {
+  $scope.images = [
+    {url: '/images/topgames/cod.jpg', done: true},
+    {url: '/images/topgames/1.jpg', done: true},
+    {url: '/images/topgames/2.jpg', done: true},
+    {url: '/images/topgames/3.jpg', done: true},
+    {url: '/images/topgames/4.jpg', done: true},
+    {url: '/images/topgames/5.jpg', done: true}
+  ]
+}
